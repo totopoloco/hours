9c0ef2c504fc8920a8f5ef4c19781024bb1f265a
Marco Tulio Ávila Cerón <marco.avila@nagarro.com>
2024-04-26 20:45:20 +0200

    none initial entry

---
+++ src/main/java/at/mavila/utilities/hours/ranges/Application.java
@@ -0,0 +1,13 @@
+package at.mavila.utilities.hours.ranges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/at/mavila/utilities/hours/ranges/ApplicationTests.java
@@ -0,0 +1,13 @@
+package at.mavila.utilities.hours.ranges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
