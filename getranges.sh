745788a928bf1c6e20d6ca489485113bf930624d
Marco Tulio Ávila Cerón <marco.avila@nagarro.com>
2024-04-29 02:06:36 +0200

    none Updates scripts

--- getranges.sh
+++ getranges.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Use curl to call the API and get the ranges
-curl -X GET -H "Pragma: no-cache" http://localhost:8384/ranges
+curl -X GET -H "Pragma: no-cache" http://localhost:8384/ranges/30
