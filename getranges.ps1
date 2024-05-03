365e5b10847ce6b97f21345f392dcaaafd1995d1
Marco Tulio Ávila Cerón <marco.avila@nagarro.com>
2024-05-03 22:38:50 +0200

    none Format nicely in table the Json response.

--- getranges.ps1
+++ getranges.ps1
@@ -1,3 +1,7 @@
 $response = Invoke-WebRequest -Uri http://localhost:8384/ranges/30
 $json = $response.Content | ConvertFrom-Json
-$json | ConvertTo-Json -Depth 100 | Write-Output
+# Display the rangeDetails array in a table format
+$json.rangeDetails | Format-Table
+
+# Display the remaining data
+$json | Select-Object -Property totalHours, totalHoursInHHMM, expectedLunchTimeInHHMM
