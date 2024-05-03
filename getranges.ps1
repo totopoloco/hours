6549129cd7c98f4232c54223bdafabd048fa7e53
Marco Tulio Ávila Cerón <marco.avila@nagarro.com>
2024-05-03 22:57:58 +0200

    none Accepts the parameter

--- getranges.ps1
+++ getranges.ps1
@@ -1,4 +1,9 @@
-$response = Invoke-WebRequest -Uri http://localhost:8384/ranges/30
+param(
+    [Parameter(Position = 0)]
+    [int]$rangeValue = 30
+)
+
+$response = Invoke-WebRequest -Uri http://localhost:8384/ranges/$rangeValue -Method Get
 $json = $response.Content | ConvertFrom-Json
 # Display the rangeDetails array in a table format with separate columns for start and end times
 $json.rangeDetails | Select-Object @{Name='start'; Expression={$_.range.start}}, @{Name='end'; Expression={$_.range.end}}, duration, durationInHours | Format-Table -AutoSize
