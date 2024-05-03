3a64295744394fe438bdb86ddae10e1252a4dca8
Marco Tulio Ávila Cerón <marco.avila@nagarro.com>
2024-05-03 22:54:37 +0200

    none pretty format the response

--- getranges.ps1
+++ getranges.ps1
@@ -1,7 +1,7 @@
 $response = Invoke-WebRequest -Uri http://localhost:8384/ranges/30
 $json = $response.Content | ConvertFrom-Json
-# Display the rangeDetails array in a table format
-$json.rangeDetails | Format-Table
+# Display the rangeDetails array in a table format with separate columns for start and end times
+$json.rangeDetails | Select-Object @{Name='start'; Expression={$_.range.start}}, @{Name='end'; Expression={$_.range.end}}, duration, durationInHours | Format-Table -AutoSize
 
 # Display the remaining data
 $json | Select-Object -Property totalHours, totalHoursInHHMM, expectedLunchTimeInHHMM
