8adce4cb959e477b12c9d9ed661047b301fe9f35
Marco Tulio Ávila Cerón <marcotavilac@pm.me>
2024-05-27 21:47:15 +0200

    none Fixes the Optional comments

--- src/test/java/at/mavila/utilities/hours/ranges/SchemaValidatorTest.java
+++ src/test/java/at/mavila/utilities/hours/ranges/SchemaValidatorTest.java
@@ -29,7 +29,84 @@ class SchemaValidatorTest {
 
     JSONObject sampleJson = new JSONObject(
         """
-                {"rangeDetails":[{"range":{"start":[2024,5,20,9,47],"end":[2024,5,20,12,54]},"duration":"03:07","durationInHours":3.12},{"range":{"start":[2024,5,20,13,24],"end":[2024,5,20,17,24]},"duration":"04:00","durationInHours":4},{"range":{"start":[2024,5,20,17,54],"end":[2024,5,20,18,29]},"duration":"00:35","durationInHours":0.58}],"totalHours":7.7,"totalHoursInHHMM":"07:42","expectedLunchTimeInHHMM":"12:54","extraComments":{"empty":true,"present":false}}
+            {
+                "rangeDetails":
+                [
+                    {
+                        "range":
+                        {
+                            "start":
+                            [
+                                2024,
+                                5,
+                                20,
+                                9,
+                                47
+                            ],
+                            "end":
+                            [
+                                2024,
+                                5,
+                                20,
+                                12,
+                                54
+                            ]
+                        },
+                        "duration": "03:07",
+                        "durationInHours": 3.12
+                    },
+                    {
+                        "range":
+                        {
+                            "start":
+                            [
+                                2024,
+                                5,
+                                20,
+                                13,
+                                24
+                            ],
+                            "end":
+                            [
+                                2024,
+                                5,
+                                20,
+                                17,
+                                24
+                            ]
+                        },
+                        "duration": "04:00",
+                        "durationInHours": 4
+                    },
+                    {
+                        "range":
+                        {
+                            "start":
+                            [
+                                2024,
+                                5,
+                                20,
+                                17,
+                                54
+                            ],
+                            "end":
+                            [
+                                2024,
+                                5,
+                                20,
+                                18,
+                                29
+                            ]
+                        },
+                        "duration": "00:35",
+                        "durationInHours": 0.58
+                    }
+                ],
+                "totalHours": 7.7,
+                "totalHoursInHHMM": "07:42",
+                "expectedLunchTimeInHHMM": "12:54",
+                "extraComments": "OK"
+            }
             """);
 
     // Validate the sample JSON against the schema
