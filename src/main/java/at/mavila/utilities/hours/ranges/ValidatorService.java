2362ef8fc24a1dc1830f2ab3c064b955df672d7a
Marco Tulio Ávila Cerón <marcotavilac@pm.me>
2024-05-27 21:15:22 +0200

    none Fixes the Optional comments

--- src/main/java/at/mavila/utilities/hours/ranges/ValidatorService.java
+++ src/main/java/at/mavila/utilities/hours/ranges/ValidatorService.java
@@ -2,25 +2,25 @@ package at.mavila.utilities.hours.ranges;
 
 import at.mavila.hours.ranges.model.Hours;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.networknt.schema.JsonSchema;
 import com.networknt.schema.ValidationMessage;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
-import java.util.stream.Collectors;
-
 @Service
 @AllArgsConstructor(onConstructor_ = @Autowired)
 public class ValidatorService {
   private static final ThreadLocal<ObjectMapper> OBJECT_MAPPER = ThreadLocal.withInitial(() -> {
     final ObjectMapper objectMapper = new ObjectMapper();
-    final JavaTimeModule javaTimeModule = new JavaTimeModule();
-    objectMapper.registerModule(javaTimeModule);
+    objectMapper.registerModule(new JavaTimeModule());
+    objectMapper.registerModule(new Jdk8Module());
     return objectMapper;
   });
   private final JsonSchema jsonSchema;
